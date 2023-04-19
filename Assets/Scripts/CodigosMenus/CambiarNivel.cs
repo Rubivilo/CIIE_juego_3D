d63409727d0196da306ea65e531fb60283e75cee
mpgomara <90265958+mpgomara@users.noreply.github.com>
2023-04-19 16:27:25 +0200

    Menus
    Menú principal y pausa/opciones

---
+++ Assets/Scripts/CodigosMenus/CambiarNivel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarNivel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CambiarEscena("Escena2");
+    }
+}
